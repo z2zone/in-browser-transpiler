00446481a5d62ec47e28a89d39133d9245a02765
z2zone <hjun1114@gmail.com>
2024-11-22 12:18:50 -0500

    test esbuild wasm trasnpiling

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,47 @@
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import * as esbuild from 'esbuild-wasm';
 
 const App = () => {
-    return <h1>hi</h1>;
+    const [userInput, setUserInput] = useState('');
+    const [code, setCode] = useState('');
+    const ref = useRef<any>();
+    
+    const esbuildService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: './esbuild.wasm'
+        });
+    }
+
+    useEffect(() => {
+        esbuildService();
+    }, []);
+
+    const handleOnClick = async () => {
+        if(!ref.current) {
+            return;
+        }
+        let result = await ref.current.transform(userInput, {
+            loader: 'jsx',
+            target: 'es2015'
+        });
+        setCode(result.code);
+    };
+
+    return (
+        <div>
+            <textarea 
+                value={userInput}
+                onChange={(e) => {setUserInput(e.target.value)}}
+            >
+            </textarea>
+            <div>
+                <button onClick={handleOnClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+    );
 }
 
 ReactDOM.render(
