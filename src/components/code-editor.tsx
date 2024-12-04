13df7c290a98948b9a36f486489cb31f33ec54db
z2zone <hjun1114@gmail.com>
2024-12-04 10:27:29 -0500

    add prettier to format editor code

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import MonacoEditor from '@monaco-editor/react';
 import prettier from 'prettier';
 import parser from 'prettier/parser-babel';
@@ -8,27 +9,44 @@ type Props = {
 }
 
 const CodeEditor: React.FC<Props> = ({initialValues, onChange}) => {
-    const onEditorDidMount = (getValue: () => string, monacoEditor: any) =>{
+    const editorRef = useRef<any>();
+    const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+        editorRef.current = monacoEditor;
         monacoEditor.onDidChangeModelContent(() => {
             onChange(getValue());
         });
     };
+
+    const handleOnClick = () =>{
+        const editorContent = editorRef.current.getModel().getValue();
+        const formattedContent = prettier.format(editorContent, {
+            parser: 'babel',
+            plugins: [parser],
+            semi: true,
+            singleQuote: true
+        });
+        editorRef.current.setValue(formattedContent);
+    };
+
     return (
-        <MonacoEditor
-            editorDidMount={onEditorDidMount}
-            value = {initialValues}
-            theme = "dark" 
-            language = "javascript" 
-            height = "400px" 
-            options = {{
-                wordWrap: "on",
-                minimap: {enabled: false},
-                folding: false,
-                fontSize: 16,
-                automaticLayout: true,
-                showUnused: false
-            }}
-        />
+        <>
+            <button onClick={handleOnClick}>Format Code</button>
+            <MonacoEditor
+                editorDidMount={onEditorDidMount}
+                value = {initialValues}
+                theme = "dark" 
+                language = "javascript" 
+                height = "400px" 
+                options = {{
+                    wordWrap: "on",
+                    minimap: {enabled: false},
+                    folding: false,
+                    fontSize: 16,
+                    automaticLayout: true,
+                    showUnused: false
+                }}
+            />
+        </>
     );
 }
 
